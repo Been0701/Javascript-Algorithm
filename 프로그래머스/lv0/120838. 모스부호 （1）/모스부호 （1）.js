fce107dc78b8a6fd06bffa5121ef1bd79bda2b89
Been0701 <108451986+Been0701@users.noreply.github.com>
2023-07-20 11:16:48 +0900

    [level 0] Title: 모스부호 (1), Time: 0.18 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120838. 모스부호 （1）/모스부호 （1）.js
@@ -0,0 +1,15 @@
+function solution(letter) {
+    var answer = '';
+    let morse = { 
+    '.-':'a','-...':'b','-.-.':'c','-..':'d','.':'e','..-.':'f',
+    '--.':'g','....':'h','..':'i','.---':'j','-.-':'k','.-..':'l',
+    '--':'m','-.':'n','---':'o','.--.':'p','--.-':'q','.-.':'r',
+    '...':'s','-':'t','..-':'u','...-':'v','.--':'w','-..-':'x',
+    '-.--':'y','--..':'z'
+}
+    let codes = letter.split(' ');
+    for (let code of codes) {
+        answer += morse[code];
+    }
+    return answer;
+}
