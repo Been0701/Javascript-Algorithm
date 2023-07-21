ac5424afe36cefacabaed604441107f89a9cc646
Been0701 <108451986+Been0701@users.noreply.github.com>
2023-07-21 11:55:26 +0900

    당연한 부분 제거

--- 프로그래머스/lv0/120844. 배열 회전시키기/배열 회전시키기.js
+++ 프로그래머스/lv0/120844. 배열 회전시키기/배열 회전시키기.js
@@ -2,9 +2,9 @@ function solution(numbers, direction) {
     // 앞에서 넣는건 unshift, 뒤에서 빼는건 pop : right
     // 앞에서 빼는건 shift, 뒤에서 넣는건 push : left
     if (direction === 'right') {
-        numbers.unshift(numbers.pop(numbers[numbers.length-1]))
+        numbers.unshift(numbers.pop())
     } else {
-        numbers.push(numbers.shift(numbers[0]))
+        numbers.push(numbers.shift())
     }
     return numbers;
 }
