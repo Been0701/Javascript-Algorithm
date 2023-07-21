288b15d3799630189293d48e8dc8c030518e0e37
Been0701 <108451986+Been0701@users.noreply.github.com>
2023-07-21 17:06:27 +0900

    [level 0] Title: 가장 큰 수 찾기, Time: 0.03 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/lv0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.js
+++ 프로그래머스/lv0/120899. 가장 큰 수 찾기/가장 큰 수 찾기.js
@@ -1,9 +1,4 @@
 function solution(array) {
-    let index = 0;
-    let max = 0;
-    for (let i=0; i<array.length; i++) {
-        max = array[i] > max ? array[i] : max;
-        if (max === array[i]) index = i;
-    }
-    return [max, index];
+    let max = Math.max(...array);
+    return [max, array.indexOf(max)];
 }
